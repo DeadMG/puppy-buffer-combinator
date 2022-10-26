040c99444d921532ba338705484a23617c05ae96
Mark Boyall <mark.boyall@bluewire-technologies.com>
2022-10-26 11:56:37 +0100

    Fix mod configuration updates causing game to fail on load

--- script/configuration.lua
+++ script/configuration.lua
@@ -1,8 +1,10 @@
+local combinator = require("script.combinator")
+
 function onConfigurationChanged()
     -- Our signals might be out of sync; re-sync them to the combinator
     if not global.entity_state then return end
     for unit_number, state in pairs(global.entity_state) do
-        if state.entity.valid then
+        if state.entity and state.entity.valid then
             combinator.applyStateToEntity(state.entity)
         end
     end 
